66df1623bf1e23ea816be3899eb3544536acbd6f
MaxX_Akela <145983198+MaxX-Akela@users.noreply.github.com>
2025-12-07 20:27:46 +0700

    Modify presetting.sh for launch file configurations
    Updated launch files to set default arguments and configure simulator.

--- scripts/presetting.sh
+++ scripts/presetting.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
 
-echo aruco.launch
+echo aruco.launch Настроен
+
 sed -i '3s|<arg name="aruco_map" default="false"/>|<arg name="aruco_map" default="true"/>|' aruco.launch
 sed -i '4s|<arg name="aruco_vpe" default="false"/>|<arg name="aruco_vpe" default="true"/>|' aruco.launch
 sed -i '7s|<arg name="map" default="map.txt"/>|<arg name="map" default="cmit.txt"/>|' aruco.launch
 
-echo clover.launch
+echo clover.launch Настроен
+
 sed -i '10s|<arg name="aruco" default="false"/>|<arg name="aruco" default="true"/>|' clover.launch
+
+dir="$(rospack find clover_simulation)/resources/worlds"
+worlds=("$dir"/*.world)
+
+random_world=${worlds[$random % ${#worlds[@]}]}
+name=$(name "$random_world")
+
+SIM_LAUNCH=$(rospack find clover_simulation)/launch/simulator.launch
+
+
+echo simulator.launch Настроен
+
+sed -i "s|<arg name=\"world_name\" value=\".*\"/>|<arg name=\"world_name\" value=\"$(rospack find clover_simulation)/resources/worlds/$name\"/>|" "$SIM_LAUNCH"
